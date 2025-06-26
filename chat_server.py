4b619b44badc787ed59402a14cdc8e4fccabbab9
vakonyakhin <vakonyakhin@gmail.com>
2025-06-26 18:57:23 +0300

    ADD CLI options

--- chat_server.py
+++ chat_server.py
@@ -2,23 +2,37 @@ import asyncio
 from datetime import datetime
 import aiofiles
 
+import configargparse
 
-async def get_messsages(date):
+
+def get_parser():
+
+    p = configargparse.ArgParser()
+    p.add('--host', env_var='HOST', help='host for connection')
+    p.add('--port', env_var='PORT', help='Port for connection')
+    p.add('-history', env_var='HISTORY_PATH', help='Path for chat history')
+
+    args = p.parse_args()
+    return args
+
+
+async def get_messsages(date, args):
     reader, _ = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        args.host, args.port)
 
     while True:
 
         data = await reader.readline()
         print(f'Received: {data.decode()!r}')
 
-        async with aiofiles.open('chat_history.txt', 'ab') as file:
+        async with aiofiles.open(args.history, 'ab') as file:
             message = f'{date} {data.decode()!r} \r'
             await file.write(message.encode())
 
 
 if __name__ == "__main__":
+    args = get_parser()
     now = datetime.now()
     formatted_date = now.strftime("[%d.%m.%Y %H:%M]")
 
-    asyncio.run(get_messsages(formatted_date))
+    asyncio.run(get_messsages(formatted_date, args))

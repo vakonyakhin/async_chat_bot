7ec46239b4c503945fda325fbf4a27323d58019f
vakonyakhin <vakonyakhin@gmail.com>
2025-08-26 22:56:02 +0300

    Add registration script

---
+++ registration.py
@@ -0,0 +1,74 @@
+import asyncio
+import logging
+import json
+
+from chat_tools import get_logger, get_parser
+
+
+async def registration(args):
+
+    """
+    Register a new user on the chat server and save the authentication token.
+
+    This function handles the registration process by:
+    - Reading initial server prompts.
+    - Sending empty input (possibly as part of the protocol).
+    - Receiving username to the user.
+    - Receiving and parsing the server's JSON response.
+    - Saving the returned token to 'token.txt' if valid.
+
+    Args:
+        reader (asyncio.StreamReader): Reader object for receiving data from the server.
+        writer (asyncio.StreamWriter): Writer object for sending data to the server.
+
+    Returns:
+        None
+
+    Raises:
+        json.JSONDecodeError: If the server's response cannot be parsed as JSON.
+    """
+
+    logger.debug('Open connection for registration')
+    reader, writer = await asyncio.open_connection(args.host, args.port)
+
+    await reader.readline()
+    logger.debug('Send empty hash')
+    writer.write(f'.. \n'.encode())
+    await writer.drain()
+    await reader.readline()
+    print('Enter preferred nickname below:')
+    await reader.readline()
+
+    username = input('')
+    logger.debug('Send prefered username for registraion')
+    writer.write(f'{username}\n'.encode())
+    await writer.drain()
+
+    response_data = await reader.readline()
+    await reader.readline()
+    print(response_data.decode())
+
+    print(
+        'Welcome to chat! For send messages in ' \
+        'chat please launch send_message.py.'
+        )
+
+    try:
+        logger.debug('Get token')
+        token = json.loads(response_data.decode())
+        if token:
+            with open('token.json', 'w') as file:
+                logger.debug('Safe token to file')
+                json.dump(token, file, indent=4)
+    except json.JSONDecodeError:
+        print('JSON decoding error')
+
+    finally:
+        logger.debug('Close connection')
+        writer.close()
+        await writer.wait_closed()
+
+if __name__ == '__main__':
+    logger = get_logger('default')
+    args = get_parser()
+    asyncio.run(registration(args))

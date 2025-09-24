ecd50bed73cb6e0b724b83c5d0b5a0527760de53
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-09-24 19:41:18 +0300

    Add main.py with queue

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+import gui
+
+
+async def main():
+
+    messages_queue = asyncio.Queue() 
+    save_messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+
+ 
+
+if __name__ == "__main__":
+    
+    asyncio.run(main())

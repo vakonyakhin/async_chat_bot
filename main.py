84ab6f5b99e006e040964edc88af75e1052a5426
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-09-24 19:44:54 +0300

    Add read message from queue

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import asyncio
-import gui
+from datetime import datetime
 
+import gui
 from chat_tools import get_logger, create_arg_parser, get_parse_arguments
 
+
+async def read_logs(filepath, queue):
+    if filepath:
+        with open(filepath, 'r') as f:
+            for line in f:
+                await queue.put(line.strip())
+    return        
+
+
+async def read_msgs(host, port, filepath, queue, save_queue):
+    reader, _ = await asyncio.open_connection(host, port)
+    if filepath:
+        await read_logs(filepath, queue)
+        print(f'Размер очереди после считывания истории сообщений {queue.qsize()}')
+        
+    while True:
+        message = await reader.readline()
+        now = datetime.now().strftime('[%d.%m.%Y %H:%M]')
+        frmt_message = f'{now} {message.decode().strip()}'
+        
+        await queue.put(frmt_message)
+        print(queue.qsize())
+        await save_queue.put(frmt_message)
+        
 
 async def main():
 

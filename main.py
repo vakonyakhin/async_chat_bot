1304f8796bef514dd4781a37b48c63cb931897bd
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-09-25 15:30:43 +0300

    Add authentification and fix send msg

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ import asyncio
 from datetime import datetime
 
 import gui
-from chat_tools import get_logger, create_arg_parser, get_parse_arguments
+from chat_tools import get_logger, create_arg_parser, get_parse_arguments, read_token
+
+
+async def authentication(reader, writer):
+    
+    token = read_token()
+    
+    logger.debug('Send token to server')
+    writer.write(f'{token['account_hash']}\n'.encode())
+
+    await writer.drain()
+
+    response_data = await reader.readline()
+    if response_data.decode().strip() == 'null':
+        print('Your token is incorrect. Please launch registration.py again')
+        return False    
+    else:
+        print(f'Выполнена авторизация. Пользователь {token['nickname']}')
+
+    return token['nickname']
 
 
 async def read_logs(filepath, queue):
@@ -14,7 +33,9 @@ async def read_logs(filepath, queue):
 
 
 async def read_msgs(host, port, filepath, queue, save_queue):
+
     reader, _ = await asyncio.open_connection(host, port)
+
     if filepath:
         await read_logs(filepath, queue)
 
@@ -36,20 +57,35 @@ async def save_messages(filepath, queue):
                 queue.task_done()
 
 
-async def send_messages(host, port, queue):
+async def send_messages(host, port, send_queue, save_queue, message_queue):
+    
+    reader, writer = await asyncio.open_connection(host, port)
+    await reader.readline()
 
+    if reader and writer:
+        logger.debug('Do authentification')
+        username = await authentication(reader, writer)
+
+    logger.debug(await reader.readline())
     while True:
-        msg = await queue.get()
-        print(f'{msg}')
+        message = await send_queue.get()
+        print(f'{message}')
+        now = datetime.now().strftime('[%d.%m.%Y %H:%M]')
+        frmt_message = f'{now} {username}: {message}'
+
+        await save_queue.put(frmt_message)
+        await message_queue.put(frmt_message)
 
 
 async def main():
 
-    config_path = ['./configs/reader.ini', './configs/sender.ini']
-    send_parser = create_arg_parser(config_path[1])
-    arg_parser = create_arg_parser(config_path[0])
-    read_arguments = get_parse_arguments(arg_parser)
+    config_path = ['./configs/reader.ini']
+    send_parser = create_arg_parser(['./configs/sender.ini'])
+    read_parser = create_arg_parser(config_path)
+    read_arguments = get_parse_arguments(read_parser)
     send_arguments = get_parse_arguments(send_parser)
+
+    reader, writer = await asyncio.open_connection(send_arguments.host, send_arguments.port)
 
     messages_queue = asyncio.Queue() 
     save_messages_queue = asyncio.Queue()
@@ -59,7 +95,8 @@ async def main():
     draw_task = asyncio.create_task(gui.draw(messages_queue, sending_queue, status_updates_queue))
     read_task = asyncio.create_task(read_msgs(read_arguments.host, read_arguments.port, read_arguments.filepath, messages_queue, save_messages_queue))
     save_task = asyncio.create_task(save_messages(read_arguments.filepath, save_messages_queue))
-    send_task = asyncio.create_task(send_messages(send_arguments.host, send_arguments.port, sending_queue))
+    send_task = asyncio.create_task(send_messages(send_arguments.host, send_arguments.port, sending_queue, save_messages_queue, messages_queue))
+    #auth_task = asyncio.create_task(authentication(reader,writer))
 
     await asyncio.gather(draw_task, read_task, save_task, send_task)
 
